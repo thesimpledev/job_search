0f83c7d9e7aaf8bf579ff0cc8f344f5647d296a3
Drew <thesimpledev@gmail.com>
2019-10-10 02:48:56 -0700

    Parse position_exclusions

--- lib/models/request_parser.rb
+++ lib/models/request_parser.rb
@@ -3,7 +3,8 @@ class RequestParser
     def parse_search_params(params)
       {
         good_keywords: parse_good_keywords(params['good_keywords']),
-        bad_keywords: parse_bad_keywords(params['bad_keywords'])
+        bad_keywords: parse_bad_keywords(params['bad_keywords']),
+        position_exclusions: parse_position_exclusions(params['position_exclusions'])
       }
     end
 
@@ -21,6 +22,8 @@ class RequestParser
       Hash[*bad_keywords.split("\r\n").map { |el| el.split(' ') }.flatten]
     end
 
-    def parse_position_exclusions(position_exclusions); end
+    def parse_position_exclusions(position_exclusions)
+      position_exclusions.split("\r\n")
+    end
   end
 end
