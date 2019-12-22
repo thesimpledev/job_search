d0456da4f1eff66e386092ffdf6f929769ccdd5c
Drew <thesimpledev@gmail.com>
2019-12-22 12:00:00 -0800

    Allow multi word matches

--- lib/models/point_allocation.rb
+++ lib/models/point_allocation.rb
@@ -1,10 +1,11 @@
 # Accumulated points from good and bad keywords matched from text
 #
 class PointAllocation
-  attr_reader :points,       # [Integer] accumulated points
-              :good_matches, # [Array<String>] good key words that were matched
-              :bad_matches,  # [Array<String>] bad key words that were matched
-              :passing_score # [Integer] score needed to pass
+  attr_reader :points,        # [Integer] accumulated points
+              :good_matches,  # [Array<String>] good key words that were matched
+              :bad_matches,   # [Array<String>] bad key words that were matched
+              :passing_score, # [Integer] score needed to pass
+              :description    # [String] text description to match against
 
   # Initialize and keep track of points, good_matches, bad matches, and passing
   # score while allocate points and parsing text to find matches
@@ -14,12 +15,12 @@ class PointAllocation
   # @param [Hash] bad_keywords with String as key and negative points as value
   # @param [Integer] passing_score is the amount of points needed to pass
   #
-  def initialize(text = '', good_keywords = {}, bad_keywords = {}, passing_score = 0)
+  def initialize(description = '', good_keywords = {}, bad_keywords = {}, passing_score = 0)
     self.points = 0
     self.bad_matches = []
     self.good_matches = []
     self.passing_score = passing_score.to_i
-    self.words_found = text
+    self.description = description.downcase
     allocate_good_matches(good_keywords)
     allocate_bad_matches(bad_keywords)
   end
@@ -45,15 +46,10 @@ class PointAllocation
 
   def match(keywords, attribute)
     keywords.each do |word, value|
-      if words_found[word]
+      if description.include?(word)
         self.points += value.to_i
         send(attribute) << word
       end
     end
-  end
-
-  def words_found=(text)
-    @words_found = {}
-    text.downcase.split(' ').each { |word| @words_found[word] = true }
   end
 end
